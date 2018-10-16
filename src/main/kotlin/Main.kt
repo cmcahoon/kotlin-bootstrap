c3f90bd09a71360a09a76e5a03ed4173d30a5dc9
Charles Cahoon <charles_cahoon@symantec.com>
2018-10-16 12:43:20 -0700

    chore: Add basic SLF4J + log4j logging configuration.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,9 @@
+import org.slf4j.LoggerFactory
+
+// Create a logger using SLF4J (Simple Logging Facade). SLF4J is designed to allow changing the logging implementation
+// at runtime while maintaining the logging interface throughout the code.
+val logger = LoggerFactory.getLogger("application")
+
 fun main (args: Array<String>) {
-    println("Hello World!")
+    logger.debug("Hello World!")
 }
